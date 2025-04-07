b64608ba6ac1da48e15864729ca5e7e7f5f0d300
Max Barboza <maxsuelstorch@gmail.com>
2025-04-07 12:25:14 -0300

    Correções

--- src/main/java/br/com/on/fiap/app/AppApplication.java
+++ src/main/java/br/com/on/fiap/app/AppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan(basePackages = "br.com.on.fiap.app.entity")
 public class AppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
