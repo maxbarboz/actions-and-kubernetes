7ea467146bd4cf4ea4dea25e8b42b6f4f10577b2
Max Barboza <maxsuelstorch@gmail.com>
2025-04-09 16:51:25 -0300

    Correções

--- src/main/java/br/com/on/app/service/PessoaService.java
+++ src/main/java/br/com/on/app/service/PessoaService.java
@@ -1,21 +1,26 @@
 package br.com.on.app.service;
 
 import br.com.on.app.component.PessoaComponent;
+import br.com.on.app.entity.PessoaEntity;
 import br.com.on.app.entity.dto.PessoaInsertDTO;
+import br.com.on.app.mapper.PessoaMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PessoaService {
 
-    private PessoaComponent component;
+    private PessoaComponent pessoaComponent;
+    private PessoaMapper pessoaMapper;
 
     @Autowired
-    public PessoaService(PessoaComponent pessoaComponent) {
-        this.component = pessoaComponent;
+    public PessoaService(PessoaComponent pessoaComponent, PessoaMapper pessoaMapper) {
+        this.pessoaComponent = pessoaComponent;
+        this.pessoaMapper = pessoaMapper;
     }
 
     public PessoaInsertDTO incluirPessoa(PessoaInsertDTO pessoa) {
-        return new PessoaInsertDTO();
+        PessoaEntity pessoaEntity = pessoaMapper.toEntity(pessoa);
+        return pessoaMapper.toDTO(pessoaComponent.save(pessoaEntity));
     }
 }
---
+++ src/main/resources/db.migration/V1__202503241737_CREATE_INDEX_TABLE_PESSOA.sql
@@ -0,0 +1,5 @@
+---------------------------------------------------------------
+------ Índices adicionais para otimizar buscas
+---------------------------------------------------------------
+CREATE INDEX idx_pessoa_nome ON tb_pes_pessoa (pes_nome);
+CREATE INDEX idx_pessoa_email ON tb_pes_pessoa (pes_email);
